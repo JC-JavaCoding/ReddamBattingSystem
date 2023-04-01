9c4679d5481e82ede7ee5c7b31bd6f9a807444c8
JC-JavaCoding <janchristiaan.jacobs@gmail.com>
2023-04-01 09:03:14 +0200

    Project initialisation, 3 main folders/aspects: 'Managers', 'UI', 'testing'.

---
+++ TeacherBattingProject/src/main/java/com/mycompany/teacherbattingproject/TeacherBattingProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teacherbattingproject;
+
+/**
+ *
+ * @author janch
+ */
+public class TeacherBattingProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
