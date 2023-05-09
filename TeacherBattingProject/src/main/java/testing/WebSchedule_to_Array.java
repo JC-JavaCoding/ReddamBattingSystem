bd535eaae9a2ccbba746d51063883e19d6f58133
JC-JavaCoding <janchristiaan.jacobs@gmail.com>
2023-05-09 23:12:52 +0200

    UI Redesign started, base layout & colour scheme
    Following problems:
    	Get schedule from online

---
+++ TeacherBattingProject/src/main/java/testing/WebSchedule_to_Array.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testing;
+
+/**
+ *
+ * @author janch
+ */
+public class WebSchedule_to_Array
+{
+    
+}
